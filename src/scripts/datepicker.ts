8dbc49a813cb22e350f3ca77f94a4f74c9073f33
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-03-07 11:20:58 +0000

    - Fixed typescript lint.

--- src/scripts/datepicker.ts
+++ src/scripts/datepicker.ts
@@ -1,5 +1,7 @@
 import '../styles/datepicker.scss';
 
+type CallbackFunction = (...args: any[]) => void;
+
 export class Datepicker {
     private options: any;
     private selector: any;
@@ -24,34 +26,14 @@ export class Datepicker {
         this.week = document.querySelector('.datepicker__week');
         this.label = document.querySelector('.datepicker__label');
 
-        type CallbackFunction = (n: any) => any;
+        this.init(() => { /** callback function */ });
 
-        this.init(() => {});
+        console.log(this.selector);
+        console.log(this.currentDay);
+        console.log(this.selectedDay);
     }
 
-    private static extend(options: any) {
-        const settings: any = {
-            selector: '.datepicker',
-            lang: 'en',
-            format: "mm/dd/yyyy",
-            weekShort: true,
-            multiplePick: true,
-            disablePastDays: false,
-            onLoad: () => {},
-            onChange: () => {},
-            onSelect: () => {}
-        };
-
-        const userSttings = options;
-        for (const attrname in userSttings) {
-            settings[attrname] = userSttings[attrname];
-        }
-
-        return settings;
-    };
-
-
-    public init(callback: Function) {
+    public init(callback: CallbackFunction) {
 
         this.date.setDate(1);
         this.updted();
@@ -59,9 +41,9 @@ export class Datepicker {
         if (callback) {
             callback.call(this);
         }
-    };
+    }
 
-    public prev(callback: Function): void {
+    public prev(callback: CallbackFunction): void {
         this.clearCalendar();
         const prevMonth = this.date.getMonth() - 1;
         this.date.setMonth(prevMonth);
@@ -71,9 +53,9 @@ export class Datepicker {
         if (callback) {
             callback.call(this);
         }
-    };
+    }
 
-    public next(callback: Function): void {
+    public next(callback: CallbackFunction): void {
         this.clearCalendar();
         const nextMonth = this.date.getMonth() + 1;
         this.date.setMonth(nextMonth);
@@ -83,11 +65,11 @@ export class Datepicker {
         if (callback) {
             callback.call(this);
         }
-    };
+    }
 
-    public selectDay(callback: Function): void {
+    public selectDay(callback: CallbackFunction): void {
         this.activeDates = document.querySelectorAll('.is-active');
-        for (let i = 0; i < this.activeDates.length; i++) {
+        for (const i of this.activeDates) {
             this.activeDates[i].addEventListener('click', (event: any) => {
                 this.selectedDay = event.target;
                 this.removeActiveClass();
@@ -96,38 +78,34 @@ export class Datepicker {
                 if (callback) {
                     callback.call(this);
                 }
-            })
+            });
         }
-    };
+    }
 
     public createMonth(): void {
         const currentMonth = this.date.getMonth();
         while (this.date.getMonth() === currentMonth) {
-            this.createDay(
-                this.date.getDate(),
-                this.date.getDay(),
-                this.date.getFullYear()
-            )
-            this.date.setDate(this.date.getDate() + 1)
+            this.createDay( this.date.getDate(), this.date.getDay(), this.date.getFullYear());
+            this.date.setDate(this.date.getDate() + 1);
         }
 
-        this.date.setDate(1)
-        this.date.setMonth(this.date.getMonth() - 1)
-        this.selectDay(() => {});
-    };
+        this.date.setDate(1);
+        this.date.setMonth(this.date.getMonth() - 1);
+        this.selectDay(() => { /** callback function */ });
+    }
 
     public creatWeek(dayShort: number) {
         const weekDay = <any>document.createElement('span');
         weekDay.className = 'datepicker__week__day';
         weekDay.innerHTML = dayShort;
         this.week.appendChild(weekDay);
-    };
+    }
 
     public createDay (num: number, day: number, year: number): void {
         const newDay = <any>document.createElement('div');
         newDay.innerHTML = num;
         newDay.className = 'datepicker__day';
-        newDay.setAttribute('data-calendar-date', this.date)
+        newDay.setAttribute('data-calendar-date', this.date);
 
         if (num === 1) {
             if (day === 0) {
@@ -152,50 +130,72 @@ export class Datepicker {
         if (this.month) {
             this.month.appendChild(newDay);
         }
-    };
+    }
 
     public monthsAsString(monthIndex: any): any {
         return this.langs.months[monthIndex];
-    };
+    }
 
     public updted(): void {
 
         this.createMonth();
-        this.readFile("./dist/langs/" + this.options.lang + ".json", (text: any) => {
+        this.readFile('./dist/langs/' + this.options.lang + '.json', (text: any) => {
             this.langs = JSON.parse(text);
             this.label.innerHTML = this.monthsAsString(this.date.getMonth()) + ' ' + this.date.getFullYear();
 
             const weekFormat = this.options.weekShort ? this.langs.daysShort : this.langs.days;
-            for (let day = 0; day < weekFormat.length; day ++) {
+            for (const day of weekFormat) {
                 this.creatWeek(weekFormat[day]);
             }
         });
-    };
+    }
 
     public clearCalendar(): void {
         this.month.innerHTML = '';
-    };
+    }
 
     public removeActiveClass(): void {
-        for (let i = 0; i < this.activeDates.length; i++) {
+        for (const i of this.activeDates) {
             this.activeDates[i].classList.remove('is-selected');
         }
-    };
+    }
 
-    public readFile(file: string, callback: Function): void {
+    public readFile(file: string, callback: CallbackFunction): void {
         const xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
+        xobj.overrideMimeType('application/json');
         xobj.open('GET', file, true);
-        xobj.onreadystatechange = function() {
-            if (xobj.readyState == 4 && <any>xobj.status == "200") {
+        xobj.onreadystatechange = () => {
+            if (xobj.readyState === 4 && <any>xobj.status === '200') {
                 callback(xobj.responseText);
             }
-        }
+        };
         xobj.send(null);
-    };
+    }
+
+    private static extend(options: any) {
+        const settings: any = {
+            selector: '.datepicker',
+            lang: 'en',
+            format: 'mm/dd/yyyy',
+            weekShort: true,
+            multiplePick: true,
+            disablePastDays: false,
+            onLoad: () => { /** callback function */ },
+            onChange: () => { /** callback function */ },
+            onSelect: () => { /** callback function */ },
+        };
+
+        const userSttings = options;
+        for (const attrname of userSttings) {
+            settings[attrname] = userSttings[attrname];
+        }
+
+        return settings;
+    }
+
 }
 
-import { Datepicker as MyDatepicker } from "./datepicker";
+import { Datepicker as MyDatepicker } from './datepicker';
 export namespace MyModule {
     export const Datepicker = MyDatepicker;
 }
