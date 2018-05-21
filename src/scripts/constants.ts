d3fee9a5041046267e2fd8002d43656327fb4c47
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-05-21 22:53:02 +0100

    - Added constants files.

---
+++ src/scripts/constants.ts
@@ -0,0 +1,26 @@
+/* @enum {string} */
+export const CSS_CLASSES = {
+    MONTH        : 'hello-week__month',
+    DAY          : 'hello-week__day',
+    WEEK         : 'hello-week__week',
+    WEEK_DAY     : 'hello-week__week__day',
+    LABEL        : 'hello-week__label',
+    IS_ACTIVE    : 'is-active',
+    IS_HIGHLIGHT : 'is-highlight',
+    IS_SELECTED  : 'is-selected',
+    IS_DISABLED  : 'is-disabled',
+    IS_TODAY     : 'is-today',
+    IS_WEEKEND   : 'is-weekend',
+};
+
+/* @enum {string} */
+export const DAYS_WEEK: any = {
+    SUNDAY    : 0,
+    MONDAY    : 1,
+    TUESDAY   : 2,
+    WEDNESDAY : 3,
+    THURSDAY  : 4,
+    FRIDAY    : 5,
+    SATURDAY  : 6,
+};
+
