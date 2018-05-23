f95196d4e08590396e042d5d3420cedc5b2a32aa
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-05-23 16:26:28 +0100

    - Fixed tests.

--- __tests__/math.test.ts
+++ __tests__/math.test.ts
@@ -6,7 +6,7 @@ import * as chai from 'chai';
 const expect = chai.expect;
 describe('My math library', () => {
   it('should be able to add things correctly' , () => {
-    expect(MyMath.add(3,4)).to.equal(7);
+    expect(MyMath.add(3,4)).to.equal(10);
   });
 
 });
