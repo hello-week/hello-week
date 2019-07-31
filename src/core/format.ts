05e7b4630da9fac9ade90c7a882a0d2e338c8b73
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-07-31 16:51:06 +0100

    fix: config import.

--- src/core/format.ts
+++ src/core/format.ts
@@ -1,5 +1,3 @@
-import { config } from "./config";
-
 export function formatDate(day: number, month: number, year: number): string {
     return `${year}-${("0" + (month + 1)).slice(-2)}-${("0" + day).slice(-2)}`;
 }
--- src/core/timestamp.ts
+++ src/core/timestamp.ts
@@ -1,3 +1,5 @@
+import { config } from "./config";
+
 /**
  * Set date timestamp to human format.
  *
