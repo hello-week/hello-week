8ac06dddc9ca4a3bc6c2d4c8f7a36b0d715d56e2
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-12-05 10:54:02 +0000

    fix: name of folder for defs.

---
+++ src/scripts/defs/iOptions.ts
@@ -0,0 +1,59 @@
+export interface IOptions {
+  selector: string;
+  langFolder: string;
+  daysHighlight: any;
+  daysSelected: any;
+  defaultDate?: string | null;
+  disableDates: any;
+  disableDaysOfWeek: any;
+  disablePastDays: boolean;
+  format: any;
+  lang: string;
+  locked: boolean;
+  maxDate: any;
+  minDate: any;
+  monthShort: boolean;
+  multiplePick: boolean;
+  nav: ['◀', '▶'];
+  range: boolean;
+  rtl: boolean;
+  todayHighlight: boolean;
+  weekShort: boolean;
+  weekStart: number;
+  onLoad: () => void;
+  onClear: () => void;
+  onNavigation: () => void;
+  onSelect: () => void;
+}
+
+export interface ICssClasses {
+  CALENDAR: string;
+  DAY: string;
+  MONTH: string;
+  NAVIGATION: string;
+  NEXT: string;
+  PERIOD: string;
+  PREV: string;
+  RTL: string;
+  WEEK: string;
+}
+
+export interface ICssStates {
+  IS_BEGIN_RANGE: string;
+  IS_DISABLED: string;
+  IS_END_RANGE: string;
+  IS_HIGHLIGHT: string;
+  IS_SELECTED: string;
+  IS_TODAY: string;
+  IS_WEEKEND: string;
+}
+
+export interface IDaysWeek {
+  FRIDAY: number;
+  MONDAY: number;
+  SATURDAY: number;
+  SUNDAY: number;
+  THURSDAY: number;
+  TUESDAY: number;
+  WEDNESDAY: number;
+}
