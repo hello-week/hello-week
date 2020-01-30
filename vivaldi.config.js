9688be6eff5f0b6969f40ee9d8c14746a3824536
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-01-30 18:28:19 +0000

    fix: add demos

--- vivaldi.config.js
+++ vivaldi.config.js
@@ -140,7 +140,17 @@ const config = {
             text: 'RTL'
           }
         ]
-      }
+      },
+      {
+        text: 'Demo',
+        children: [
+          {
+            path: 'demos/00-selected-days.md',
+            slug: 'selected-days',
+            text: 'Selected Days'
+          },
+          ]
+         }
     ],
     v3: [
       {
