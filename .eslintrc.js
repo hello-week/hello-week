118ec007f91411cb11d6c84f02ce2489ea1ea767
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-03-28 19:22:59 +0100

    style: add `tsdoc` plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
     extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ]
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+        'tsdoc/syntax': 'warn',
+    },
 };
